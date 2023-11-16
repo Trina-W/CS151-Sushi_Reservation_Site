350d8b42feb9649692a1aff910ffe3a8d29a1598
Katrina Weers <katrinaweers@gmail.com>
2023-11-16 11:00:23 -0800

    adding cancelres and edit mainGUI

---
+++ Reservation.java
@@ -0,0 +1,25 @@
+
+import java.time.LocalDate;
+
+public class Reservation {
+    private String reservationID;
+    private String customerName;
+    private LocalDate time;
+    private double phoneNum;
+    private String email;
+
+    /**
+     * Creates a Reservation at a restaurant 
+     * @param name name of the customer
+     * @param em customer email
+     * @param pn customer phone number
+     * @param time time of the reservation
+     */
+    public Reservation(String name, String em, double pn, LocalDate time) {
+        this.customerName = name;
+        this.email = em;
+        this.phoneNum = pn;
+        this.time = time;
+        // Generate random string for reservationID
+    }
+}
